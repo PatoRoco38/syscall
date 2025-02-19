1f664954ce2e3fad2d4c1934d829f8f1a174eefd
PatoRoco38 <renato.lancelot@gmail.com>
2025-02-19 14:51:11 -0300

    Finalizado as migrações do banco de dados

--- core/models.py
+++ core/models.py
@@ -1,28 +1,40 @@
 from django.db import models
+from django.utils.timezone import now
 
 # Create your models here.
 class Chamado(models.Model):
-    id = models.Model(int, primary_key=True)
-    numero = models.Model(str, unique=True, max_length=10, nullable=False)
-    posto = models.Model(str, nullable=False, max_length=100)
-    telefone = models.Model(str, nullable=False, max_length=15)
-    ipv4 = models.Model(str, nullable=False, max_length=15)
-    solicitante = models.Model(str, nullable=False, max_length=100)
-    status = models.Model(str, nullable=False, default="Aberto")
-    acao = models.Model(str, nullable=True, max_length=255)
-    resposta = models.Model(str, nullable=True)
+    STATUS_CHOICES = [
+        ("Aberto", "Aberto"),
+        ("Em atendimento", "Em atendimento"),
+        ("Fechado", "Fechado"),
+    ]
+
+    id = models.AutoField(primary_key=True)
+    posto = models.CharField(default="Não informado", max_length=100)
+    telefone = models.CharField(default="62999999999", null=False, max_length=15)
+    ipv4 = models.GenericIPAddressField(default="0.0.0.0", max_length=15)
+    solicitante = models.CharField(default="Solicitante", null=False, max_length=100)
+    status = models.CharField(choices=STATUS_CHOICES, default="Aberto", max_length=15)
+    acao = models.CharField(null=True, max_length=255)
+    resposta = models.CharField(null=True, max_length=500)
     descricao = models.CharField(max_length=255)
     data_criacao = models.DateTimeField(auto_now_add=True)
-    data_fechamento = models.DateTimeField(auto_now_add=True)
+    data_fechamento = models.DateTimeField(null=True, blank=True)
+    def save(self, *args, **kwargs):
+
+        # Se o chamado acaba de ser fechado e não tem data de fechamento, define a data atual
+        if self.status == "Fechado" and self.data_fechamento is None:
+            self.data_fechamento = now()
+        return super().save(*args, **kwargs)
 
 
 class Usuario(models.Model):
-    id = models.Model(int, primary_key=True)
-    nome_posto = models.Model(str, max_length=100, nullable=False)
-    nome_usuario = models.Model(str, max_length=100, nullable=False)
-    email_unidade = models.Model(str, unique=True, nullable=False)
-    telefone = models.Model(str, max_length=15, nullable=False)
-    tipo = models.Model(str, nullable=False, max_length=20, default="Cliente")
+    id = models.AutoField(primary_key=True)
+    nome_posto = models.CharField(default="unidade ou posto", max_length=100, null=False)
+    nome_usuario = models.CharField(max_length=100, null=False)
+    email_unidade = models.CharField(unique=True, null=False, max_length=100)
+    telefone = models.CharField(max_length=15, null=False)
+    tipo = models.CharField(null=False, max_length=20, default="Cliente")
 
     def __str__(self):
         return self.descricao
